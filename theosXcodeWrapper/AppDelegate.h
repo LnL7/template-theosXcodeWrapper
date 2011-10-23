e0a0ca29ea93154797426d15e0db81ae595dad9c
hBook <daiderd@hotmail.com>
2011-10-23 16:34:58 +0200

    Initial Commit

---
+++ theosXcodeWrapper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  theosXcodeWrapper
+//
+//  Created by L0CKnL0aD7 on 23/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
